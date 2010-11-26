189ac51f505fc4cead59ec5c5a302b5a71b8269f
testaddict <eric@macbook-pro-de-eric-mignot-3.local>
2010-11-26 10:52:24 -0500

    starting green field

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hibou::Application.config.secret_token = '8669c1ad94042a0f733725857b468adc7f8ccc58ae50d8ad504d835d4733922b71292e5b5764136ad17decf392b26262e32d7d8dded2b3b4b384b3be273b62d9'
