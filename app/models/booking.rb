914713649ac38b11e014555e6fac7e228aec4d92
testaddict <eric@macbook-pro-de-eric-mignot-3.local>
2010-11-27 04:11:13 -0500

    validations on booking file

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ActiveRecord::Base
   belongs_to :child
   validates_inclusion_of :period, :in => %w( AM PM )
+  validates_presence_of :date, :child
 
   def Booking::find_am_bookings date
     Booking.where("date = ? AND period = 'AM'", date)
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,15 +2,38 @@ require 'spec_helper'
 
 describe Booking do
 
+
+  describe "validation rules" do
+
+    before(:each) do
+      @child                    = Child.create(:first_name => 'Calvin', :last_name => 'Hobbes', :birth_date => Date.today)
+      @booking_valid_attributes = {:child => @child, :period => 'AM', :date => Date.today}
+    end
+
+    def booking_with(attributes={})
+      Booking.new(@booking_valid_attributes.merge!(attributes))
+    end
+
+    it "is considered invalid without a period" do
+      booking_with(:period => '').should_not be_valid
+    end
+    it "is considered invalid without a date" do
+      booking_with(:date => '').should_not be_valid
+    end
+    it "is considered invalid without a child" do
+      booking_with(:child => nil).should_not be_valid
+    end
+  end
+
   describe "structure" do
     it "should require the period to be either AM or PM" do
-      roger = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
+      roger   = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
       booking = Booking.create :child => roger, :date => "2010-11-27", :period => 'unknown'
       booking.save.should be_false
     end
 
     it "should allow AM as the period" do
-      roger = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
+      roger   = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
       booking = Booking.create :child => roger, :date => "2010-11-27", :period => 'AM'
       booking.save.should be_true
     end
@@ -18,18 +41,18 @@ describe Booking do
 
   describe "find_am_bookings" do
     it "should fetch all the AM bookings for a specific date" do
-      roger = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
-      elvis = Child.new :first_name=> 'Elvis', :last_name=> 'Gratton'
-      casper = Child.new :first_name=> 'Casper', :last_name=> 'Ghost'
-      children = [roger,elvis,casper]
-      children.each{|child| child.save}
+      roger    = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
+      elvis    = Child.new :first_name=> 'Elvis', :last_name=> 'Gratton'
+      casper   = Child.new :first_name=> 'Casper', :last_name=> 'Ghost'
+      children = [roger, elvis, casper]
+      children.each { |child| child.save }
 
-      am_booking = Booking.new :child => roger, :date => "2010-11-27", :period => 'AM'
-      pm_booking = Booking.new :child => elvis, :date => "2010-11-27", :period => 'PM'
+      am_booking                 = Booking.new :child => roger, :date => "2010-11-27", :period => 'AM'
+      pm_booking                 = Booking.new :child => elvis, :date => "2010-11-27", :period => 'PM'
       am_booking_for_another_day = Booking.new :child => roger, :date => "2010-11-28", :period => 'AM'
 
-      bookings_array = [am_booking_for_another_day, am_booking,pm_booking]
-      bookings_array.each {|booking| booking.save}
+      bookings_array             = [am_booking_for_another_day, am_booking, pm_booking]
+      bookings_array.each { |booking| booking.save }
       bookings = Booking.find_am_bookings "2010-11-27"
       bookings.should include(am_booking)
 
@@ -39,18 +62,18 @@ describe Booking do
 
   describe "find_pm_bookings" do
     it "should fetch all the PM bookings for a specific date" do
-      roger = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
-      elvis = Child.new :first_name=> 'Elvis', :last_name=> 'Gratton'
-      casper = Child.new :first_name=> 'Casper', :last_name=> 'Ghost'
-      children = [roger,elvis,casper]
-      children.each{|child| child.save}
+      roger    = Child.new :first_name=> 'Roger', :last_name=> 'Moquin'
+      elvis    = Child.new :first_name=> 'Elvis', :last_name=> 'Gratton'
+      casper   = Child.new :first_name=> 'Casper', :last_name=> 'Ghost'
+      children = [roger, elvis, casper]
+      children.each { |child| child.save }
 
-      am_booking = Booking.new :child => roger, :date => "2010-11-27", :period => 'AM'
-      pm_booking = Booking.new :child => elvis, :date => "2010-11-27", :period => 'PM'
+      am_booking                 = Booking.new :child => roger, :date => "2010-11-27", :period => 'AM'
+      pm_booking                 = Booking.new :child => elvis, :date => "2010-11-27", :period => 'PM'
       am_booking_for_another_day = Booking.new :child => roger, :date => "2010-11-28", :period => 'AM'
 
-      bookings_array = [am_booking_for_another_day, am_booking,pm_booking]
-      bookings_array.each {|booking| booking.save}
+      bookings_array             = [am_booking_for_another_day, am_booking, pm_booking]
+      bookings_array.each { |booking| booking.save }
       bookings = Booking.find_pm_bookings "2010-11-27"
       bookings.should include(pm_booking)
     end
