0f1d69956affe2e82598e74ec9d80c1ed1c302ea
testaddict <eric.mignot@gmail.com>
2011-01-17 20:32:54 -0500

    using rails dynamic finders

--- features/pages/bookings_page.rb
+++ features/pages/bookings_page.rb
@@ -1,8 +1,8 @@
 When /^(?:|I) cancel the child ([^"]*) ([^"]*) for ([^"]*) ([^"]*)/ do |first_name, last_name, date, period|
-  child_id = Child.where( :first_name => first_name, :last_name => last_name).first.id
-  booking_id = Booking.where( :child_id => child_id, :date => date, :period => period).first.id
-  
-  find(:xpath, "//a[@href='/bookings/#{booking_id}' and @data-method='delete']").click
+  child = Child.find_by_first_name_and_last_name(first_name, last_name)
+  booking = Booking.find_by_child_id_and_date_and_period(child.id, date, period)
+
+  find(:xpath, "//a[@href='/bookings/#{booking.id}' and @data-method='delete']").click
 end
 
 def booking_p(child, date, period)
