2cf3944fae5d28e1499137eacb90507611281124
testinfected <vtence@gmail.com>
2011-01-18 00:20:43 -0500

    We don't have pdf-inspector in vendor/lib, it's packaged as a gem by bundler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'vendor','pdf-inspector','lib')
 require "pdf/inspector"
 require 'rspec/rails'
 require 'webrat'
